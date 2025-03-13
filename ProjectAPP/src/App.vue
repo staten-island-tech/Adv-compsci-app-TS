107e92d3f2c978058ec5fc0d9a30ef873d88c617
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2025-03-13 19:53:08 -0400

    Country Info Search
    - Cleaned up the page to include search bar and button
    - still need to organize where the actual data will go and how it will look
    -base code is done for the ability to enter a country name and get its data back
    - should include some error handing later since it has none right now
    - fixed the over head menu bar for now, although its a but crude of methodolgy

--- ProjectAPP/src/App.vue
+++ ProjectAPP/src/App.vue
@@ -4,13 +4,18 @@
 
 <main>
 
-<div>
-  <nav>
-<RouterLink to="/test">testing</RouterLink>
-<RouterLink to="/">landing</RouterLink>
-</nav>
-<RouterView />
+<div class="MenuBar">
+  <nav class="nav">
+    <RouterLink to="/test" class="LinkOne">testing</RouterLink>
+    <RouterLink to="/" class="LinkTwo">landing</RouterLink>
+  </nav>
 </div>
+
+<div class="RouterViewContent">
+  <RouterView />
+</div>
+
+
 </main>
 
 
@@ -32,7 +37,36 @@ import landing from './views/landing.vue'
 
 
 <style>
+.MenuBar{
+  position: fixed; 
+  top: 0;
+  left: 50%;  
+  transform: translateX(-50%); 
+  width: 100vw; 
+  height: 4vh;  
+  background-color: #000000;  
+  display: flex;  
+  justify-content: center;  
+  align-items: center;  
 
 
+}
+.LinkOne, .LinkTwo /* If you need to add more router buttons just link them here so they get autoadjusted into the menu bar */{
+  color: rgb(255, 255, 255);  
+  text-decoration: none;  
+  padding-left: 3vw;
+  padding-right: 3vw;
+
+}
+
+.nav{
+  display: flex;  
+  gap: 1vw;
+
+}
+
+.RouterViewContent {
+  margin-top: 5vh; /*  Makes sure that all the content called form the router button will appear below the menu bar so it doesnt clip through */ 
+}
 
 </style>
--- ProjectAPP/src/views/HomeView.vue
+++ ProjectAPP/src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
-    <button @click="fetchCountryInfo">API Call button</button>
-    <button @click="console.log(thingy)">log info</button>  <!-- this wont return anything until the other button is pressed as that is what actually retunrs the API data into the thingy object(or const whatever it is) -->
+
+    <form class="Search_Action" @submit.prevent="Search(Country), Country = ''">
+      <input type="text" placeholder="Enter a country" v-model="Country" >
+      <button type="submit" >Search</button>
+    </form>
+  
     <p v-if="thingy">Name: {{ thingy.capital }}</p>
     <p v-if="thingy">Sex Ratio: {{ thingy.sex_ratio }}</p>
   </template>
@@ -11,12 +15,35 @@
   
   const thingy = ref<{ capital?: string; sex_ratio?:number } | null>(null);  // reactive var | just add in more of the data names from the object to hold them in the template
   
-  const fetchCountryInfo = async () => {
-    thingy.value = await getCountryInfo('United States');  // async funtion that returnsv value to thingy so I can acces them with the ref const above with reactive things in them
+  const fetchCountryInfo = async (Name:string) => {
+    thingy.value = await getCountryInfo(Name);  // async funtion that returnsv value to thingy so I can acces them with the ref const above with reactive things in them
   };
+
+
+
+  const Country = ref<string>('')
+  
+  function Search(x:string , ) {
+    if (x == ''){
+      console.log("no input")
+    }
+    else{
+      thingy.value = null // hides the p tags with the v-if"thingy" model so that prevouous country data isnt show after entering a new search
+      fetchCountryInfo(x)
+      console.log(x)
+    }
+
+    
+
+    
+  }
   </script>
   
   <style scoped>
-  /* Add your styles here */
+
+  .Search_Action{
+    margin-top: 2vh;
+  }
+
   </style>
   
