799b1ba7705220f4137f7bcdcd7d633a06cab47c
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2025-03-16 18:45:23 -0400

    .

--- ProjectAPP/src/router/index.ts
+++ ProjectAPP/src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Landing from '@/views/landing.vue'
+import FilterCountry from '@/views/FilterCountry.vue'
+import GuessCountry from '@/views/GuessCountry.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/test',
+      path: '/CountryInfo',
       name: 'test',
       component: HomeView,
     },
@@ -15,6 +17,17 @@ const router = createRouter({
       name: 'landing',
       component: Landing,
     },
+    {
+      path: '/Countries by filter',
+      name: 'Filter',
+      component: FilterCountry,
+    },
+    {
+      path: '/',
+      name: 'GuessCountry',
+      component: GuessCountry,
+    },
+
     
   ],
 })
---
+++ ProjectAPP/src/views/GuessCountry.vue
@@ -0,0 +1,10 @@
+<template>
+    <main>
+      <p>Guess countries page</p>
+    </main>
+  </template>
+  
+  <script setup lang="ts"></script>
+  
+  <style></style>
+  
